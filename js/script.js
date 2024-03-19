c041cc6eb304ffa1e4d79e4b99a9ec0d5f95e76a
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-19 18:50:24 +0000

    start html css js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+/**
+ * This function gets users street name and street number and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetname = document.getElementById("street-name").value
+  const streetnumber = parseInt(document.getElementById("street-number").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + streetname + streetnumber + "."
+}
